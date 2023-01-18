27b910d12758aff9dbffbde79e61af5388a21fe8
Ouail-01 <ouaillaarabi@gmail.com>
2023-01-18 21:05:42 +0100

    Fix the linters

--- src/index.js
+++ src/index.js
@@ -3,34 +3,34 @@ import './style.css';
 const todoList = document.querySelector('.todo-list');
 
 const taskStorage = [
-    {
-        id: 0,
-        description: 'Watch some courses',
-        completed: true,
-    },
-    {
-        id: 1,
-        description: 'Watch some tuto',
-        completed: true,
-    },
-    {
-        id: 2,
-        description: 'Complete the week',
-        completed: false,
-    },
+  {
+    id: 0,
+    description: 'Watch some courses',
+    completed: true,
+  },
+  {
+    id: 1,
+    description: 'Watch some tuto',
+    completed: true,
+  },
+  {
+    id: 2,
+    description: 'Complete the week',
+    completed: false,
+  },
 ];
 
 const autoFillData = () => {
-    let show = '';
-    taskStorage.forEach((i) => {
-        show += `<li class="task-item" id="${i.id}">
+  let show = '';
+  taskStorage.forEach((i) => {
+    show += `<li class="task-item" id="${i.id}">
         <div>
             <input type="checkbox" class="list-input" id="${i.id} ${i.completed ? 'checked' : ''}">
             <label type="text" class="list-label" for="${i.id}" id="${i.id}">${i.description}</label>
         </div>
         <i class="fas fa-ellipsis-v resize" id="${i.id}"></i>
         </li>`;
-    });
-    todoList.innerHTML = show;
+  });
+  todoList.innerHTML = show;
 };
 autoFillData();
