67d14e68c84f27d4c0543e66577ae274e0129ca7
Ouail-01 <ouaillaarabi@gmail.com>
2023-01-18 19:45:51 +0100

    Add the tasks dynamicly

--- src/index.js
+++ src/index.js
@@ -19,3 +19,18 @@ const taskStorage = [
         completed: false,
     },
 ];
+
+const autoFillData = () => {
+    let show = '';
+    taskStorage.forEach((i) => {
+        show += `<li class="task-item" id="${i.id}">
+        <div>
+            <input type="checkbox" id="${i.id} ${i.completed ? 'checked' : ''}">
+            <label type="text" for="${i.id}" id="${i.id}">${i.description}</label>
+        </div>
+        <i class="fas fa-ellipsis-v resize" id="${i.id}"></i>
+        </li>`;
+    });
+    todoList.innerHTML = show;
+};
+autoFillData();
