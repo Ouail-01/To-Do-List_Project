b4b4bd6e85f5749d426ab5c63d8fc65e4c37c603
Ouail-01 <ouaillaarabi@gmail.com>
2023-01-18 19:44:41 +0100

    Add the task obj

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.css';
+
+const todoList = document.querySelector('.todo-list');
+
+const taskStorage = [
+    {
+        id: 0,
+        description: 'Watch some courses',
+        completed: true,
+    },
+    {
+        id: 1,
+        description: 'Watch some tuto',
+        completed: true,
+    },
+    {
+        id: 2,
+        description: 'Complete the week',
+        completed: false,
+    },
+];
